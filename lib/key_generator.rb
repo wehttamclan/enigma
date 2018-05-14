a01a0b2ca359d4a06687fe5e7e27ff951c1da813
Darian Garcia <Garcia.Darian.Lee@gmail.com>
2018-05-14 16:59:08 -0600

    Refactors the KeyGenerator class to be less lines of code

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,25 +1,14 @@
 class KeyGenerator
-  attr_reader :mac_address
-
-  def initialize
-    @mac_address = get_mac_address_from_shell
-  end
 
   def get_mac_address_from_shell
     `ifconfig en0 | grep -e "ether" | cut -d ' ' -f 2 | tr -d ':'`
   end
 
-  def convert_hex_to_decimal(num_string)
-    num_string = mac_address.to_i(16)
+  def convert_hex_to_decimal
+    get_mac_address_from_shell.to_i(16)
   end
 
-  def get_first_five(num)
-    num = mac_address.to_s[0..4].to_i
+  def get_first_five
+    convert_hex_to_decimal.to_s[0..4].to_i
   end
-
-  # def determine_if_comp_is_mac
-  # end
-  #
-  # def
-  # end
 end
