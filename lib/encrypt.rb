0c165e21d0b961f0d804b25c1bd513a665777a38
Darian Garcia <Garcia.Darian.Lee@gmail.com>
2018-05-16 13:39:43 -0600

    Fixes the encrypt and decrypt runners to work with previous dates and not have to call /data/ when working with text files

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,6 +1,7 @@
 require './lib/file_manager'
 require './lib/enigma'
 require './lib/key_generator'
+require 'pry'
 
 e = Enigma.new
 f = FileManager.new
@@ -10,5 +11,4 @@ my_message = f.open_file
 encrypted_message = e.encrypt(my_message)
 f.write_file(encrypted_message)
 #3zxeh07w26meou633pnxo5sxi
-
-# print the key to the terminal so decrypt can use it.
+puts "Created #{f.assign_filenames[1]} with the key #{keygen.key}"
