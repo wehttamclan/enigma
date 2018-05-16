e9f867d01ccbfd7cc4c623d4715246fe64612387
Darian Garcia <Garcia.Darian.Lee@gmail.com>
2018-05-15 22:11:32 -0600

    WIP - adds methods in crack class to be implemented in future commits

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,4 +1,29 @@
-require './lib/enigma'
+require './file_manager.'
 
-if ARGV[0].exist? && ARGV[1].exist?
-  
+class Crack
+
+  def crack
+
+  end
+
+  def calc(hash)
+    f = FileManager.new
+    end_str = ".end"
+    encrypted_str = f.open_file
+  end
+
+  def permutate
+    permutations = ["A", "B", "C", "D"].permutation(4) #[[],[]]
+  end
+
+  def iterate
+    permutations.each do |perm|
+      4.times do |i|
+        if perm[i] ==
+      end
+    end
+
+    def assign_hash(letter)
+      value = encrypted
+    end
+  end
