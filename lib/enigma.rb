68ea8b1c3ef3022c29b2dce965a2078b1038d374
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-05-16 19:25:03 -0600

    fixed decrypt method so key is required.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -27,36 +27,37 @@ class Enigma
   end
 
   def encrypt(plaintext, key=keygen.key, today=date_today)
-    total_shift = total_shift(key)
-    cipher(plaintext, total_shift, 1)
+    shift = total_shift(key)
+    cipher(plaintext, total_shift(key), 1)
   end
 
-  def decrypt(encrypted_text, key=keygen.key, today=date_today)
-    total_shift = total_shift(key)
-    cipher(encrypted_text, total_shift, -1)
+  def decrypt(encrypted_text, key, today=date_today)
+    shift = total_shift(key)
+    cipher(encrypted_text, total_shift(key), -1)
   end
 
-  def cipher(input_text, total_shift, i)
-    map_length = char_map.length # 39
+  def cipher(input_text, shift, i)
+    map_length = char_map.length # 85
     output_text = ""
 
     input_text.length.times do |x|
       char_map_position = char_map.key(input_text[x])
       cipher_char_map_position =
-            (i*i*char_map_position + i*total_shift[x % 4]) % map_length
+            (i*i*char_map_position + i*shift[x % 4]) % map_length
       output_text += char_map[cipher_char_map_position]
+      # require 'pry'; binding.pry
     end
     output_text
   end
-end # end class
+end
 
 
 
-# e = Enigma.new
-# p e.char_map
-# message = "this is so secret ..end.."
-# p new_key = e.keygen.key
-# p ciphertext = e.encrypt(message, key=new_key)
-# p e.decrypt(ciphertext, key=new_key)
-# p e.keygen.rotations(e.keygen.key)
+e = Enigma.new
+p k = e.keygen.key
+# p e.keygen.rotations(k)
 # p e.offset
+# p e.total_shift(k)
+message = "1234567890qwertyuiop[]asdfghjkl;zxcvbnm,. !@#$%^&*()[],.<>;:/?\|"
+p ciphertext = e.encrypt(message, key = k)
+p e.decrypt(ciphertext, key = k)
