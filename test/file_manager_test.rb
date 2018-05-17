94bb826070442261e41540f767a69c189f098bcd
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-05-16 22:15:18 -0600

    deletes whitespace.

--- test/file_manager_test.rb
+++ test/file_manager_test.rb
@@ -1,7 +1,7 @@
 require './test/test_helper'
 require './lib/file_manager.rb'
 
-class FileManagerTest  < Minitest::Test
+class FileManagerTest < Minitest::Test
   def test_files_exist
     assert File.exist?('./data/encrypted.txt')
     assert File.exist?('./data/message.txt')
